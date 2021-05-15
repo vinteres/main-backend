d83d88d671fa84f15fc966bb87f074ec4f8af121
Velizar Ivanov <zivanof@gmail.com>
2021-05-15 08:02:39 +0300

    Added translations for cyrillic to latin in locations search

--- app/repositories/location_repository.js
+++ app/repositories/location_repository.js
@@ -1,3 +1,9 @@
+const chars = { "Ё": "YO", "Й": "I", "Ц": "TS", "У": "U", "К": "K", "Е": "E", "Н": "N", "Г": "G", "Ш": "SH", "Щ": "SCH", "З": "Z", "Х": "H", "Ъ": "'", "ё": "yo", "й": "i", "ц": "ts", "у": "u", "к": "k", "е": "e", "н": "n", "г": "g", "ш": "sh", "щ": "sch", "з": "z", "х": "h", "ъ": "'", "Ф": "F", "Ы": "I", "В": "V", "А": "a", "П": "P", "Р": "R", "О": "O", "Л": "L", "Д": "D", "Ж": "ZH", "Э": "E", "ф": "f", "ы": "i", "в": "v", "а": "a", "п": "p", "р": "r", "о": "o", "л": "l", "д": "d", "ж": "zh", "э": "e", "Я": "Ya", "Ч": "CH", "С": "S", "М": "M", "И": "I", "Т": "T", "Ь": "'", "Б": "B", "Ю": "YU", "я": "ya", "ч": "ch", "с": "s", "м": "m", "и": "i", "т": "t", "ь": "'", "б": "b", "ю": "yu" };
+
+const translate = (s) => s.split('')
+  .map((c) => chars[c] || c)
+  .join('');
+
 class LocationRepository {
   constructor(conn) {
     this.conn = conn
@@ -48,8 +54,8 @@ class LocationRepository {
     text[0] = text[0].toUpperCase()
     text = text.join('') 
 
-    const query = 'SELECT * FROM cities WHERE name LIKE $1'
-    const result = await this.conn.query(query, [text + '%'])
+    const query = 'SELECT * FROM cities WHERE name LIKE $1 OR name LIKE $2'
+    const result = await this.conn.query(query, [text + '%', translate(text) + '%'])
 
     return result.rows
   }
